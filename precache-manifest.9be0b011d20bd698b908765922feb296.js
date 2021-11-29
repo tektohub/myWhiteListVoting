43a30014e90a35d36d756847311715145fb62cb1
tektohub <boudouros@gmail.com>
2021-11-29 20:53:32 +0100

    Updates

---
+++ precache-manifest.9be0b011d20bd698b908765922feb296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ded29fe2d00414beb0921348282130",
+    "url": "/WhiteList-on-heroku/index.html"
+  },
+  {
+    "revision": "f3a0bd0f32970e9a39fa",
+    "url": "/WhiteList-on-heroku/static/css/2.6a23f8e0.chunk.css"
+  },
+  {
+    "revision": "e68d157ec41fd1e2294f",
+    "url": "/WhiteList-on-heroku/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f3a0bd0f32970e9a39fa",
+    "url": "/WhiteList-on-heroku/static/js/2.d609261b.chunk.js"
+  },
+  {
+    "revision": "e68d157ec41fd1e2294f",
+    "url": "/WhiteList-on-heroku/static/js/main.421d4b39.chunk.js"
+  },
+  {
+    "revision": "1fe38f8a820ca1fb5672",
+    "url": "/WhiteList-on-heroku/static/js/runtime-main.820d075c.js"
+  }
+]);
